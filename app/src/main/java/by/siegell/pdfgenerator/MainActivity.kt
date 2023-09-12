dfb67975b44e7e5661e877366dee5af6aad708ef
pzinkevich42 <143994239+pzinkevich42@users.noreply.github.com>
2023-09-12 08:43:36 +0300

    Добавил использование viewBinding вместо findViewById. (#1)

--- app/src/main/java/by/siegell/pdfgenerator/MainActivity.kt
+++ app/src/main/java/by/siegell/pdfgenerator/MainActivity.kt
@@ -1,26 +1,28 @@
 package by.siegell.pdfgenerator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import by.siegell.pdfgenerator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var loanAgreementButton: Button
     private val testDataFactory = TestDataFactory()
+
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        loanAgreementButton = findViewById(R.id.loan_agreement_button)
-        loanAgreementButton.setOnClickListener {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.loanAgreementButton.setOnClickListener {
             openDetailActivity(
                 testDataFactory.getTestLoanAgreementData()
             )
         }
     }
 
-    fun openDetailActivity(loanAgreementData: LoanAgreementData) {
+    private fun openDetailActivity(loanAgreementData: LoanAgreementData) {
         startActivity(
             DetailActivity.createIntent(this, loanAgreementData)
         )
