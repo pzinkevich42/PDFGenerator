ad387412421de870d7717ac8a2eba728d292e541
pzinkevich42 <143994239+pzinkevich42@users.noreply.github.com>
2023-09-13 22:29:43 +0300

    Добавил использование Recycler View. (#2)

--- app/src/main/java/by/siegell/pdfgenerator/TestDataFactory.kt
+++ app/src/main/java/by/siegell/pdfgenerator/TestDataFactory.kt
@@ -2,8 +2,18 @@ package by.siegell.pdfgenerator
 
 class TestDataFactory {
 
+    fun getTestDocumentTypes(): List<DocumentTypePresentationData> {
+        return listOf(
+            DocumentTypePresentationData(
+                typeName = "Loan Agreement",
+                payload = getTestLoanAgreementData()
+            )
+        )
+    }
+
     fun getTestLoanAgreementData(): LoanAgreementData {
         return LoanAgreementData(
+            type = Type.LOAN_AGREEMENT,
             number = 5,
             dateRu = "12.04.5678",
             dateEn = "<<12>>th April 5678",
